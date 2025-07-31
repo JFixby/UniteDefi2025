17b47b0707edf4998212ff6476ac6ba697d6842b
J Fixby <github@jfixby.com>
2025-07-31 11:18:13 -0300

    vars

--- eth-side/test-print.ts
+++
@@ -1,8 +0,0 @@
-import * as variables from './variables';
-
-console.log('Testing variables import...');
-console.log('NETWORK:', variables.NETWORK);
-console.log('RPC_URLS:', variables.RPC_URLS);
-console.log('ALICE_PRIVATE_KEY exists:', !!variables.ALICE_PRIVATE_KEY);
-console.log('CAROL_PRIVATE_KEY exists:', !!variables.CAROL_PRIVATE_KEY);
-console.log('DEV_PORTAL_API_TOKEN exists:', !!variables.DEV_PORTAL_API_TOKEN); 
